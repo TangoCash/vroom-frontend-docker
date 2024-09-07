30eea6e3c798871b2a6eef3a141c53474c3967fc
TangoCash <eric@loxat.de>
2024-09-07 10:32:38 +0200

    add env variables to api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://localhost',
-  port: '3000',
+  host: process.env.API_HOST || 'http://localhost', 
+  port: process.env.API_PORT || '3000',
   maxJobNumber: 1000
 };
